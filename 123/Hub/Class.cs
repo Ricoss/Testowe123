cd7c87c528898e940b19a383a170a20054de19e1
ricoss123 <ricoss123@gmail.com>
2020-04-01 14:18:26 +0200

    Add project files.

---
+++ 123/Hub/Class.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.All.SendAsync("SendMessage", name, message);
+        }
+    }
+
+}
