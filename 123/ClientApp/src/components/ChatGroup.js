0361860bdc54ed308711e2fb8add1078cf833798
Ricoss <urbaniecmateusz2@gmail.com>
2020-04-06 17:43:15 +0200

    123

--- 123/ClientApp/src/components/ChatGroup.js
+++ 123/ClientApp/src/components/ChatGroup.js
@@ -132,7 +132,7 @@ export class ChatGroup extends Component {
 
                     <Col>
                         <div>
-                            
+                            <h2>ll</h2>
                         </div>
                     </Col>
                 </Row>
